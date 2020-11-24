9d179c4b6c2014ab55b436ac360fbbe9ceaafd93
Ivan Aristov <apostrof1995@gmail.com>
2020-11-25 00:07:47 +0300

    Perms use foldl

--- Perms.hs
+++ Perms.hs
@@ -2,11 +2,8 @@ module Perms where
 
 perms :: [a] -> [[a]]
 perms []     = [[]]
-perms (x:xs) = go xs [[x]]
+perms (x:xs) = foldl (\acc r -> concatMap (ins r [] []) acc) [[x]] xs
   where
-    go []       acc = acc
-    go (r : rs) acc = go rs $ concatMap (ins r [] []) acc
-  
     ins e acc   pr []        = reverse (e : pr) : acc
     ins e [[]]  pr ss@(s:sf) = ins e [reverse (e : pr) ++ ss]         (s : pr) sf
     ins e acc   pr ss@(s:sf) = ins e ((reverse (e : pr) ++ ss) : acc) (s : pr) sf
