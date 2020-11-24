f9035b4b60b3f8ead23ec35d0bbaee5e2c0a8c25
Ivan Aristov <apostrof1995@gmail.com>
2020-11-25 00:12:02 +0300

    Perms uniq

--- Perms.hs
+++ Perms.hs
@@ -1,5 +1,7 @@
 module Perms where
 
+import Data.List (nub)
+
 perms :: [a] -> [[a]]
 perms []     = [[]]
 perms (x:xs) = foldl (\acc r -> concatMap (ins r [] []) acc) [[x]] xs
@@ -7,3 +9,7 @@ perms (x:xs) = foldl (\acc r -> concatMap (ins r [] []) acc) [[x]] xs
     ins e acc   pr []        = reverse (e : pr) : acc
     ins e [[]]  pr ss@(s:sf) = ins e [reverse (e : pr) ++ ss]         (s : pr) sf
     ins e acc   pr ss@(s:sf) = ins e ((reverse (e : pr) ++ ss) : acc) (s : pr) sf
+
+
+permsU :: Eq a => [a] -> [[a]]
+permsU = nub . perms
